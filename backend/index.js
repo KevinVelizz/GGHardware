db36033dadb2074485f4a03fba8cd0248f1b2cd6
KevinVelizz <kevinveliz121@outlook.es>
2026-02-12 03:42:20 -0300

    test

--- backend/index.js
+++ backend/index.js
@@ -54,8 +54,8 @@ app.get("/login", async (req, res) => {
     //const [rows] = await UserModels.insertUser(name, email, password);
 
     // Con la contraseña hasheada
-    const sql = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
-    return connection.query(sql, ["matias", "matias@matias.com", hashedPassword]);
+    const sql = `INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)`;
+    return connection.query(sql, [3, "matias", "matias@matias.com", hashedPassword]);
 
 });
 
